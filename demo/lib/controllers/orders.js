b2a6f17225fbf245366f603274b047323387abed
Casey Warren <caseywar1017@gmail.com>
2021-03-25 19:00:04 -0400

    got rid of asyncs i dont need anymore

--- demo/lib/controllers/orders.js
+++ demo/lib/controllers/orders.js
@@ -3,31 +3,31 @@ const Order = require('../models/Order');
 const OrderService = require('../services/OrderService');
 
 module.exports = Router()
-  .post('/', async (req, res, next) => {
+  .post('/', (req, res, next) => {
     OrderService.create(req.body)
       .then((order) => res.send(order))
       .catch(next);
   })
 
-  .get('/', async (req, res, next) => {
+  .get('/', (req, res, next) => {
     Order.selectAll()
       .then((orders) => res.json(orders))
       .catch(next);
   })
 
-  .get('/:id', async (req, res, next) => {
+  .get('/:id', (req, res, next) => {
     Order.selectById(req.params.id)
       .then((order) => res.json(order))
       .catch(next);
   })
 
-  .put('/:id', async (req, res, next) => {
+  .put('/:id', (req, res, next) => {
     OrderService.updateEdited(req.params.id, req.body)
       .then((order) => res.json(order))
       .catch(next);
   })
 
-  .delete('/:id', async (req, res, next) => {
+  .delete('/:id', (req, res, next) => {
     OrderService.deletedOrder(req.params.id, req.body)
       .then((order) => res.json(order))
       .catch(next);
